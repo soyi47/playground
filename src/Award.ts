23c9d86d88c9b806064a982e2963b9d9ce4a8131
전소이 <soyi.jeon@woowahan.com>
2024-02-19 21:05:39 +0900

    refactor: Award 내 createRestartButton 로직 분리

--- src/Award.ts
+++ src/Award.ts
@@ -5,20 +5,24 @@ interface AwardConstructorParams {
 class Award {
   private container = document.getElementById('award') as HTMLDivElement;
   private winnerNameText: HTMLParagraphElement;
-  private restartButton: HTMLButtonElement;
+  private restartButton!: HTMLButtonElement;
 
   constructor({ onRestart }: AwardConstructorParams) {
     this.winnerNameText = document.createElement('p');
 
+    this.createRestartButton({ onRestart });
+  }
+
+  init() {
+    this.container.innerHTML = '';
+  }
+
+  createRestartButton({ onRestart }: AwardConstructorParams) {
     this.restartButton = document.createElement('button');
     this.restartButton.innerText = '다시 시작하기';
     this.restartButton.addEventListener('click', function () {
       onRestart();
     });
-  }
-
-  init() {
-    this.container.innerHTML = '';
   }
 
   showWinner(winnerNames: string[]) {
